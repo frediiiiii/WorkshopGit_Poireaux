9244e08b0d0957e9c2f91c0ab8d4405d5f77371c
poireau4 <32650287+poireau4@users.noreply.github.com>
2017-11-02 00:09:07 +0100

    preparation to include api

--- pet_sitting_index.js
+++ pet_sitting_index.js
@@ -6,3 +6,14 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+// On démarre babel, un outil qui permet d'utiliser les dernières évolutions du
+// langage Javascript (ici l'évolution ES2015) en restant compatible avec tous
+// les serveurs et navigateurs
+
+/*if (process.env.NODE_ENV === "development") {
+  require("babel-register");
+  require("./src/server");
+} else {
+  require("./lib/server");
+}
